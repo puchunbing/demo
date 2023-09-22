c2b5968e7fc4a2f63aaf0c4f2a9619dc377f6906
pcb <565504294@qq.com>
2023-09-22 16:29:00 +0800

    feat:增加关联修改

--- controllers/user.go
+++ controllers/user.go
@@ -46,10 +46,13 @@ func (this *UserControllers) AddUser(c *gin.Context) {
 
 func (this *UserControllers) GetUser(c *gin.Context) {
 	var userinfo model.UserInfo
+	//var user model.User
 	db := config.GetMySql()
 	//db.Model(&userinfo).Association("users").Find(&userinfo.User)
 	//查询info_id=1的数据放入userinfo中，并关联查询到User字段对应的数据
 	db.Preload("User").Find(&userinfo, "info_id = ?", 1)
+	//把user字段的数据放到user结构体中
+	//db.Debug().Model(&userinfo).Related(&user,"User")
 	msg := &logs.ResultInfo{}
 	logs.Logger.Debug("挂了")
 	logs.Logger.Info("第一级别")
@@ -57,5 +60,16 @@ func (this *UserControllers) GetUser(c *gin.Context) {
 	msg.Code = "0"
 	msg.Message = "查询成功"
 	msg.Data = userinfo
+	c.JSONP(200, msg)
+}
+
+func (this *UserControllers) Update(c *gin.Context) {
+	db := config.GetMySql()
+	var userinfo model.UserInfo
+	db.Preload("User").Find(&userinfo, "info_id =?", 1)
+	db.Model(&userinfo.User).Update("age", 29)
+	msg := &logs.ResultInfo{}
+	msg.Code = "0"
+	msg.Message = "修改成功"
 	c.JSONP(200, msg)
 }
--- router/routes.go
+++ router/routes.go
@@ -10,6 +10,7 @@ func CollectRoute(r *gin.Engine) *gin.Engine {
 	r.POST("/upload", controllers.UploadControllersInstance.Upload) //文件上传
 	r.POST("/user", controllers.UserControllersInstance.AddUser)    //新增
 	r.GET("/user", controllers.UserControllersInstance.GetUser)     //查询
+	r.PUT("/user", controllers.UserControllersInstance.Update)      //修改
 
 	return r
 }
